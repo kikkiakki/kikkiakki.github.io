5d818a4b60d5a3101d5c44f2d9ad4deb3b223550
Petra <43611603+kikkiakki@users.noreply.github.com>
2025-01-03 22:39:15 -0500

    Test eleventy Image util to generate thumbnails

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,20 @@
+const Image = require("@11ty/eleventy-img");
 const fg = require('fast-glob');
 const paintingImages = fg.sync(["paintings/*.{jpg,png}"]);
 
 module.exports = function (eleventyConfig) {
     // Passthrough copy paintings to _site
-	eleventyConfig.addPassthroughCopy("paintings");
-    eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+	// eleventyConfig.addPassthroughCopy("paintings");
+    // eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 
     // Add paintings collection
     eleventyConfig.addCollection("paintings", function() {
-        return paintingImages
+        return paintingImages.map(async (src) => (
+            await Image(src, {
+                widths: [400],
+                formats: ["jpeg"]
+            })
+        ))
       });
 };
 
