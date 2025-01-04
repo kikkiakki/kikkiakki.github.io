c30b123a46449c931eb0f003627886a9b0037d9b
Petra <43611603+kikkiakki@users.noreply.github.com>
2025-01-03 22:57:18 -0500

    passthrough copy img to _site

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,20 +1,20 @@
 const Image = require("@11ty/eleventy-img");
 const fg = require('fast-glob');
-const paintingImages = fg.sync(["paintings/*.{jpg,png}"]);
+const paintingImages = fg.sync(["paintings/*.{jpg,png}"]).map(
+    async (src) => await Image(src, {
+        widths: [300],
+        formats: ["jpeg"]
+    }));
 
 module.exports = function (eleventyConfig) {
     // Passthrough copy paintings to _site
-	// eleventyConfig.addPassthroughCopy("paintings");
-    // eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+    // eleventyConfig.addPassthroughCopy("paintings");
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 
     // Add paintings collection
     eleventyConfig.addCollection("paintings", function() {
-        return paintingImages.map(async (src) => (
-            await Image(src, {
-                widths: [400],
-                formats: ["jpeg"]
-            })
-        ))
+        return paintingImages
       });
 };
 
